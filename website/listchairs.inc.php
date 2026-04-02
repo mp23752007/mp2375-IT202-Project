903ccc0d4d09bfff5cd7c52c5095894a6b553ee7
mp23752007 <mp2375@njit.edu>
2026-04-01 21:11:42 -0400

    Dropdown menu added

--- website/listchairs.inc.php
+++ website/listchairs.inc.php
@@ -14,32 +14,45 @@ require_once('database.php');
 require_once('chair.php');
 $db = getDB();
 $res = Chair::listAll($db);
+
+// Save results to an array
+$chairs = [];
+while ($row = $res->fetch_assoc()) {
+    $chairs[] = $row;
+}
 ?>
 <h2>Chairs Inventory</h2>
+
+<form action="index.php" method="get">
+    <input type="hidden" name="content" value="updatechair">
+    <select name="chairID">
+        <?php foreach ($chairs as $row): ?>
+            <option value="<?= $row['chair_id'] ?>"><?= $row['chair_name'] ?> - $<?= number_format($row['chair_sell_price'], 2) ?></option>
+        <?php endforeach; ?>
+    </select>
+    <input type="submit" value="Select Chair">
+</form>
+
+<br>
+<hr><br>
 
 <table border="1" cellpadding="8" style="border-collapse: collapse; width: 100%; text-align: left;">
     <tr style="background-color: #f2f2f2;">
         <th>ID</th>
         <th>Code</th>
         <th>Name</th>
-        <th>Description</th>
         <th>Material</th>
         <th>Style</th>
-        <th>Type ID</th>
-        <th>Buy Price</th>
         <th>Sell Price</th>
     </tr>
-    <?php while ($row = $res->fetch_assoc()): ?>
+    <?php foreach ($chairs as $row): ?>
         <tr>
             <td><?= $row['chair_id'] ?></td>
             <td><?= $row['chair_code'] ?></td>
             <td><?= $row['chair_name'] ?></td>
-            <td><?= htmlspecialchars($row['chair_description']) ?></td>
             <td><?= $row['chair_material'] ?></td>
             <td><?= $row['chair_style'] ?></td>
-            <td><?= $row['chair_type_id'] ?></td>
-            <td>$<?= number_format($row['chair_buy_price'], 2) ?></td>
             <td>$<?= number_format($row['chair_sell_price'], 2) ?></td>
         </tr>
-    <?php endwhile; ?>
+    <?php endforeach; ?>
 </table>
--- website/listchairtypes.inc.php
+++ website/listchairtypes.inc.php
@@ -7,12 +7,29 @@ Assignment: Phase 3
 Email: mp2375
 */
 if (!isset($_SESSION['login'])) { echo "<h2>Error: You must be logged in.</h2>"; return; }
-require_once('database.php'); 
-require_once('chairtype.php');
+require_once('database.php'); require_once('chairtype.php');
 $db = getDB(); 
 $res = ChairType::listAll($db);
+
+// Save results to an array so we can loop through it twice (once for dropdown, once for table)
+$types = [];
+while($row = $res->fetch_assoc()) {
+    $types[] = $row;
+}
 ?>
 <h2>Chair Categories</h2>
+
+<form action="index.php" method="get">
+    <input type="hidden" name="content" value="displaychairtype">
+    <select name="typeID">
+        <?php foreach($types as $row): ?>
+            <option value="<?= $row['chair_type_id'] ?>"><?= $row['chair_type_name'] ?> (<?= $row['chair_type_code'] ?>)</option>
+        <?php endforeach; ?>
+    </select>
+    <input type="submit" value="Select Category">
+</form>
+
+<br><hr><br>
 
 <table border="1" cellpadding="8" style="border-collapse: collapse; width: 100%; text-align: left;">
     <tr style="background-color: #f2f2f2;">
@@ -21,12 +38,12 @@ $res = ChairType::listAll($db);
         <th>Category Name</th>
         <th>Aisle Number</th>
     </tr>
-    <?php while($row = $res->fetch_assoc()): ?>
+    <?php foreach($types as $row): ?>
     <tr>
         <td><?= $row['chair_type_id'] ?></td>
         <td><?= $row['chair_type_code'] ?></td>
         <td><?= $row['chair_type_name'] ?></td>
         <td><?= $row['chair_aisle_number'] ?></td>
     </tr>
-    <?php endwhile; ?>
+    <?php endforeach; ?>
 </table>
